5214bd2162633c20b5e4a06f1150f5560807c77b
arafat22184 <123alarafat@gmail.com>
2026-04-25 22:43:27 +0600

    feat: added all products from api

---
+++ script/home.js
@@ -0,0 +1,44 @@
+const productContainer = document.getElementById("productContainer");
+
+const fetchProducts = () => {
+  fetch("https://in-a5-server.vercel.app/products")
+    .then((res) => res.json())
+    .then((data) => {
+      displayProducts(data);
+    });
+};
+
+const displayProducts = (products) => {
+  products.forEach((product) => {
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+    <div
+            class="bg-white drop-shadow-lg p-4 rounded-xl border-t-2 border-[#4F46E5]"
+          >
+            <img
+              src=${product.img}
+              alt=${product.title}
+              class="rounded-xl h-46 w-full object-cover"
+            />
+
+            <div class="space-y-2 my-3">
+              <p class="font-medium">${product.title}</p>
+              <p
+                class="bg-[#4F46E5]/20 text-[#4F46E5] px-3 py-0.5 w-fit rounded-xl"
+              >
+                ${product.category}
+              </p>
+              <p class="text-[#4F46E5]">$${product.price}</p>
+            </div>
+
+            <button class="bg-[#4F46E5] text-white py-2 w-full rounded-xl">
+              View Details
+            </button>
+          </div>
+    `;
+    productContainer.appendChild(card);
+  });
+};
+
+fetchProducts();
