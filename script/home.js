e9488a9a52bc1b3c4007eb66e83d00bf4407963d
arafat22184 <123alarafat@gmail.com>
2026-04-25 23:20:16 +0600

    feat: selected category dataaa showed

--- script/home.js
+++ script/home.js
@@ -1,13 +1,31 @@
 const productContainer = document.getElementById("productContainer");
+const select = document.getElementById("select");
 const loading = document.getElementById("loading");
 let isLoading = true;
 
-const fetchProducts = () => {
+const fetchProducts = (selectedValue) => {
   isLoading = true;
   loading.style.display = "flex";
+
   fetch("https://in-a5-server.vercel.app/products")
     .then((res) => res.json())
     .then((data) => {
+      if (selectedValue) {
+        if (selectedValue === "All") {
+          displayProducts(data);
+          isLoading = false;
+          loading.style.display = "none";
+          return;
+        }
+        const filteredData = data.filter(
+          (product) => product.category === selectedValue,
+        );
+
+        displayProducts(filteredData);
+        isLoading = false;
+        loading.style.display = "none";
+        return;
+      }
       displayProducts(data);
       isLoading = false;
       loading.style.display = "none";
@@ -15,12 +33,14 @@ const fetchProducts = () => {
 };
 
 const displayProducts = (products) => {
+  productContainer.innerHTML = "";
+
   products.forEach((product) => {
     const card = document.createElement("div");
 
     card.innerHTML = `
     <div
-            class="bg-white drop-shadow-lg p-4 rounded-xl border-t-2 border-[#4F46E5]"
+            class="bg-white drop-shadow-lg p-4 rounded-xl border-t-2 ${product.category === "Men" ? "border-[#4F46E5]" : product.category === "Women" ? "border-[#FF33EA]" : "border-orange-400"}"
           >
             <img
               src=${product.img}
@@ -31,7 +51,7 @@ const displayProducts = (products) => {
             <div class="space-y-2 my-3">
               <p class="font-medium">${product.title}</p>
               <p
-                class="bg-[#4F46E5]/20 text-[#4F46E5] px-3 py-0.5 w-fit rounded-xl"
+                class="px-3 py-0.5 w-fit rounded-xl ${product.category === "Men" ? "bg-[#4F46E5]/20 text-[#4F46E5] " : product.category === "Women" ? "bg-[#FF33EA]/20 text-[#FF33EA]" : "bg-orange-400/20 text-orange-400"}"
               >
                 ${product.category}
               </p>
@@ -46,5 +66,13 @@ const displayProducts = (products) => {
     productContainer.appendChild(card);
   });
 };
+
+select.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  const selectedValue = e.target.value;
+
+  fetchProducts(selectedValue);
+});
 
 fetchProducts();
